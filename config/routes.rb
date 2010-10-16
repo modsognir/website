a98ad61daf199876dc02681377c1d600f3ff873b
jaredfraser <j.fraser@modsognir.com>
2010-10-16 18:16:35 +1100

    Removed placeholder index

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Modsognir::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "articles#index"
-  root :to => "site#index"
+  root :to => "articles#index"
   # See how all your routes lay out with "rake routes"
   
   # This is a legacy wild controller route that's not recommended for RESTful applications.
